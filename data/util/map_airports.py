e6fa648b8cb47eb69c95c8d04deb1fb0520e435c
Ethan Pelkie <epelk@umich.edu>
2023-02-15 22:52:54 -0500

    Debug map_airports.py and add airports.csv
    - Fix minor bugs in airport mapping code.
    - Incorporate new source for airport lat/lon; old source was inaccurate.
    - Old source provided location of weather stations, not airports.
    - This was problematic when stations were outside the US.

--- data/util/map_airports.py
+++ data/util/map_airports.py
@@ -11,6 +11,20 @@ airport is dropped from the data.
 
 mappings = []
 
+icao_coords = []
+# From https://ourairports.com/data/
+with open('airports.csv', 'r', encoding='utf-8') as f_airport_db:
+    for l in f_airport_db.readlines():
+        tokens = l.split(',')
+        # Check country code. Remove surrounding quotes.
+        if tokens[8][1:-1] in ['US', 'PR', 'UM', 'AS', 'GU', 'MP', 'VI']:
+            icao_coords.append({
+                # Remove surrounding quotes from ICAO
+                'icao': tokens[1][1:-1],
+                'lat': tokens[4],
+                'lon': tokens[5]
+            })
+
 # From https://transtats.bts.gov/Fields.asp?gnoyr_VQ=FGK (key OriginAirportID)
 with open('L_AIRPORT.csv', 'r') as f_airport_faa_ids:
     # From https://en.wikipedia.org/wiki/List_of_airports_in_the_United_States
@@ -33,16 +47,29 @@ with open('L_AIRPORT.csv', 'r') as f_airport_faa_ids:
                     station_found = False
                     for station in meteostat:
                         if station['identifiers']['icao'] == icao:
-                            mappings.append({
-                                # trim closing " and \n
-                                'desc': ''.join(line.split(',')[1:])[1:-2],
-                                'icao': icao,
-                                'faa': line.split(',')[0][1:-1],
-                                'mstat': station['id'],
-                                'location': station['location']
-                            })
-                            station_found = True
-                            success += 1
+                            location = {}
+                            for coord_obj in icao_coords:
+                                if coord_obj['icao'] == icao:
+                                    location['lat'] = coord_obj['lat']
+                                    location['lon'] = coord_obj['lon']
+                                    break
+
+                            if location == {}:
+                                fail += 1
+                                station_found = True
+                                # print(f'fail _{icao}_')
+
+                            else:
+                                mappings.append({
+                                    # trim closing " and \n
+                                    'desc': ''.join(line.split(',')[1:])[1:-2],
+                                    'icao': icao,
+                                    'faa': line.split(',')[0][1:-1],
+                                    'mstat': station['id'],
+                                    'location': location
+                                })
+                                station_found = True
+                                success += 1
 
                     if not station_found:
                         # print(f'no weather station found for icao {icao}')
@@ -52,17 +79,29 @@ with open('L_AIRPORT.csv', 'r') as f_airport_faa_ids:
                     # print(f'no icao found for faa {line.split(",")[0][1:-1]}')
                     fail += 1
 
-                print(f'success: {success} fail: {fail}')
+                # print(f'success: {success} fail: {fail}')
 
 # From https://transtats.bts.gov/Fields.asp?gnoyr_VQ=FGK (key Origin)
+final_mappings = []
 with open('L_AIRPORT_ID.csv') as f_bts_ids:
     idlines = f_bts_ids.readlines()
+    s = 0
+    f = 0
     for m in mappings:
+        found = False
         for l in idlines:
             if m['desc'] == ''.join(l.split(',')[1:])[1:-2]:
                 m['bts_id'] = l.split(',')[0][1:-1]
+                s += 1
+                found = True
+                final_mappings.append(m)
 
-print(f'generated {len(mappings)} records')
+        if not found:
+            f += 1
+
+    # print(f's {s} f {f}')
+
+print(f'generated {len(final_mappings)} records')
 
 with open('../airport_mappings.json', 'w') as f_out:
-    json.dump(mappings, f_out, indent=2)
+    json.dump(final_mappings, f_out, indent=2)
