bae9c38705079368f607d1921b1b063643417552
Drew-Barlow <79282784+Drew-Barlow@users.noreply.github.com>
2023-03-26 21:21:45 -0400

    Updated GUI with input and sliders

--- intelliflight/GUI.py
+++ intelliflight/GUI.py
@@ -15,19 +15,62 @@ class App(customtkinter.CTk):
         self.title("IntelliFlight")
         self.geometry("900x600")
 
-        self.grid_rowconfigure((0, 1, 2), weight=1)
-        self.grid_columnconfigure((0,1,2), weight=1)
-        
-        self.logo_label = customtkinter.CTkLabel(master=self, text="Welcome to IntelliFlight", font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.logo_label.grid(row=0, column=1, padx =20, pady=(20,0), sticky="nsew")
+        self.grid_rowconfigure((0), weight=1)
+        self.grid_columnconfigure((0), weight=1)
 
-        self.button = customtkinter.CTkButton(master=self, command=self.button_callback)
-        self.button.grid(row=1,column=1,padx=20, pady=20, sticky="ew")
+        self.tabview = customtkinter.CTkTabview(self, width=250)
+        self.tabview.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        self.tabview.add("Train")
+        self.tabview.add("Predict")
+        self.tabview.tab("Train").grid_columnconfigure((0),weight=1)
+        self.tabview.tab("Predict").grid_columnconfigure((0),weight=1)
 
+        #Training data input
+        self.button = customtkinter.CTkButton(self.tabview.tab("Train"),text="Input training data" ,command=self.button_callback)
+        self.button.grid(row=0,column=0,padx=20,pady=20)
+
+        #Partition count
+        self.labalP = customtkinter.CTkLabel(self.tabview.tab("Train"),text="Use the slider to set the partition count")
+        self.labalP.grid(row=1,column=0)
+        self.sliderP = customtkinter.CTkSlider(self.tabview.tab("Train"), from_=0, to=100, number_of_steps=10, command=self.sliderP_callback)
+        self.sliderP.grid(row=2,column=0)
+        self.labelOutputP = customtkinter.CTkEntry(self.tabview.tab("Train"),width=50,height=50)
+        self.labelOutputP.grid(row=3,column=0)
+
+        #k value increment
+        self.labelK = customtkinter.CTkLabel(self.tabview.tab("Train"),text="Use the slider to set the increment for each k value")
+        self.labelK.grid(row=4,column=0)
+        self.sliderK = customtkinter.CTkSlider(self.tabview.tab("Train"),from_=0, to=100, number_of_steps=10, command=self.sliderK_callback)
+        self.sliderK.grid(row=5,column=0)
+        self.labelOutputK = customtkinter.CTkEntry(self.tabview.tab("Train"),width=50,height=50)
+        self.labelOutputK.grid(row=6,column=0)
+
+        #Max k fraction
+        self.labelF = customtkinter.CTkLabel(self.tabview.tab("Train"),text="Use the slider to set the percentage of the max fraction")
+        self.labelF.grid(row=7,column=0)
+        self.SliderF = customtkinter.CTkSlider(self.tabview.tab("Train"),from_=0, to=1, command=self.sliderF_callback)
+        self.SliderF.grid(row=8,column=0)
+        self.labelOutputF = customtkinter.CTkEntry(self.tabview.tab("Train"),width=50, height=50)
+        self.labelOutputF.grid(row=9,column=0)
 
     def button_callback(self):
-        print("button pressed")
+        dialog = customtkinter.CTkInputDialog(text="Enter training data file path:", title="Training Data")
+        print("Test:", dialog.get_input())
 
+    def sliderP_callback(self,value):
+        value = self.sliderP.get()
+        self.labelOutputP.insert(0,str(value))
+        print(value)
+
+    def sliderK_callback(self, value):
+        value = self.sliderK.get()
+        self.labelOutputK.insert(0,str(value))
+        print(value)
+    
+    def sliderF_callback(self,value):
+        value = self.SliderF.get()
+        self.labelOutputF.insert(0,str(value))
+        print(value)
 
 if __name__ =="__main__":
     app = App()
