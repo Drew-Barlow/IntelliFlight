aa5f99b63f2280af9289f7d5f4061f98b2bf25ce
Drew-Barlow <79282784+Drew-Barlow@users.noreply.github.com>
2023-03-29 23:43:05 -0400

    Adjusted gui

--- intelliflight/GUI.py
+++ intelliflight/GUI.py
@@ -22,49 +22,74 @@ class App(customtkinter.CTk):
         self.tabview.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
         self.tabview.add("Train")
         self.tabview.add("Predict")
-        self.tabview.tab("Train").grid_columnconfigure((0,1,2,3,4,5,6,7,8,9),weight=1)
+        self.tabview.tab("Train").grid_columnconfigure((0,1,2,3,4),weight=1)
         self.tabview.tab("Predict").grid_columnconfigure((0),weight=1)
-
-        #Training data input
-        self.inputLabel = customtkinter.CTkLabel(self.tabview.tab("Train"),text="Use this button to input the file path for the training data:")
-        self.inputLabel.grid(row=0,column=0)
-        self.button = customtkinter.CTkButton(self.tabview.tab("Train"),text="Input training data" ,command=self.button_callback)
-        self.button.grid(row=0,column=1,pady=(20))
-        self.trainingCheckbox = customtkinter.CTkCheckBox(self.tabview.tab("Train"),text="")
-        self.trainingCheckbox.grid(row=0,column=2)
         
-        #Partition count
-        self.partitionLabel = customtkinter.CTkLabel(self.tabview.tab("Train"),text="Use the slider to set the partition count:")
-        self.partitionLabel.grid(row=1,column=0)
-        self.sliderP = customtkinter.CTkSlider(self.tabview.tab("Train"), from_=0, to=100, number_of_steps=10, command=self.sliderP_callback)
+        #################################################################
+        #Set text frame
+        self.textFrame = customtkinter.CTkFrame(self.tabview.tab("Train"))
+        self.textFrame.grid(row=0,column=0,pady=20,sticky='nsew')
+        #Set training data input text
+        self.inputLabel = customtkinter.CTkLabel(self.textFrame,text="Use this button to input the file path for the training data:")
+        self.inputLabel.grid(row=0,column=0,pady=20,padx=20)
+        #Set partition count text
+        self.partitionLabel = customtkinter.CTkLabel(self.textFrame,text="Use the slider to set the partition count:")
+        self.partitionLabel.grid(row=1,column=0,pady=20,padx=20)
+        #Set k value increment text
+        self.kValueLabel = customtkinter.CTkLabel(self.textFrame,text="Use the slider to set the increment for each k value:")
+        self.kValueLabel.grid(row=2,column=0,pady=20,padx=20)
+        #Set max k value fraction text
+        self.kFractionLabel = customtkinter.CTkLabel(self.textFrame,text="Use the slider to set the percentage of the max fraction:")
+        self.kFractionLabel.grid(row=3,column=0,pady=20,padx=20)
+        #################################################################
+
+
+        #################################################################
+        #Set input frame
+        self.inputFrame = customtkinter.CTkFrame(self.tabview.tab("Train"))
+        self.inputFrame.grid(row=0,column=2,pady=20,sticky='nsew')
+        #Set training data input button
+        self.button = customtkinter.CTkButton(self.inputFrame,text="Input training data" ,command=self.button_callback)
+        self.button.grid(row=0,column=1,pady=20,padx=20)
+        #Set partition count input slider
+        self.sliderP = customtkinter.CTkSlider(self.inputFrame, from_=0, to=100, number_of_steps=10, command=self.sliderP_callback)
+        self.sliderP.grid(row=1,column=1,pady=27,padx=20)
         self.sliderP.set(0)
-        self.sliderP.grid(row=1,column=1)
-        self.partitionOutputLabel = customtkinter.CTkEntry(self.tabview.tab("Train"),width=37,height=30)
-        self.partitionOutputLabel.grid(row=1,column=2)
-        self.partitionCheckbox = customtkinter.CTkCheckBox(self.tabview.tab("Train"),text="")
-        self.partitionCheckbox.grid(row=1,column=3)
-
-        #k value increment
-        self.kValueLabel = customtkinter.CTkLabel(self.tabview.tab("Train"),text="Use the slider to set the increment for each k value:")
-        self.kValueLabel.grid(row=2,column=0)
-        self.sliderK = customtkinter.CTkSlider(self.tabview.tab("Train"),from_=0, to=100, number_of_steps=10, command=self.sliderK_callback)
-        self.sliderK.grid(row=2,column=1)
-        self.sliderK.set(10)
-        self.kValueOutput = customtkinter.CTkEntry(self.tabview.tab("Train"),width=37,height=30)
+        self.partitionOutput = customtkinter.CTkEntry(self.inputFrame,width=37,height=30)
+        self.partitionOutput.grid(row=1,column=2)
+        #Set k value increment slider
+        self.sliderK = customtkinter.CTkSlider(self.inputFrame,from_=0, to=100, number_of_steps=10, command=self.sliderK_callback)
+        self.sliderK.grid(row=2,column=1,pady=27,padx=20)
+        self.sliderK.set(0)
+        self.kValueOutput = customtkinter.CTkEntry(self.inputFrame,width=37,height=30)
         self.kValueOutput.grid(row=2,column=2)
-        self.kValueCheckbox = customtkinter.CTkCheckBox(self.tabview.tab("Train"),text="")
-        self.kValueCheckbox.grid(row=2,column=3)
-
-        #Max k fraction
-        self.kFractionLabel = customtkinter.CTkLabel(self.tabview.tab("Train"),text="Use the slider to set the percentage of the max fraction:")
-        self.kFractionLabel.grid(row=3,column=0)
-        self.SliderF = customtkinter.CTkSlider(self.tabview.tab("Train"),from_=0, to=1, command=self.sliderF_callback)
-        self.SliderF.grid(row=3,column=1)
+        #Set max k value fraction slider
+        self.SliderF = customtkinter.CTkSlider(self.inputFrame,from_=0, to=1, command=self.sliderF_callback)
+        self.SliderF.grid(row=3,column=1,pady=27,padx=20)
         self.SliderF.set(0)
-        self.kFractionOutput = customtkinter.CTkEntry(self.tabview.tab("Train"),width=40, height=30)
+        self.kFractionOutput = customtkinter.CTkEntry(self.inputFrame,width=37, height=30)
         self.kFractionOutput.grid(row=3,column=2)
-        self.kFractionCheckbox = customtkinter.CTkCheckBox(self.tabview.tab("Train"),text="")
-        self.kFractionCheckbox.grid(row=3,column=3)
+        #################################################################
+
+
+        #################################################################
+        #Set progress frame
+        self.progressFrame = customtkinter.CTkFrame(self.tabview.tab("Train"))
+        self.progressFrame.grid(row=0,column=4,pady=20,sticky='nsew')
+        #Set training data checkbox
+        self.trainingCheckbox = customtkinter.CTkCheckBox(self.progressFrame,text="")
+        self.trainingCheckbox.grid(row=0,column=3,pady=27)
+        #Set partition count checkbox
+        self.partitionCheckbox = customtkinter.CTkCheckBox(self.progressFrame,text="")
+        self.partitionCheckbox.grid(row=1,column=3,pady=27)
+        #Set k value increment checkbox
+        self.kValueCheckbox = customtkinter.CTkCheckBox(self.progressFrame,text="")
+        self.kValueCheckbox.grid(row=2,column=3,pady=27)
+        #Set max k fraction chechbox
+        self.kFractionCheckbox = customtkinter.CTkCheckBox(self.progressFrame,text="")
+        self.kFractionCheckbox.grid(row=3,column=3,pady=27)
+        #################################################################
+
 
     def button_callback(self):
         dialog = customtkinter.CTkInputDialog(text="Enter training data file path:", title="Training Data")
@@ -73,8 +98,8 @@ class App(customtkinter.CTk):
 
     def sliderP_callback(self,value):
         value = self.sliderP.get()
-        self.partitionOutputLabel.delete(0,10)
-        self.partitionOutputLabel.insert(0,str(value))
+        self.partitionOutput.delete(0,10)
+        self.partitionOutput.insert(0,str(value))
         self.partitionCheckbox.select()
         print(value)
         
