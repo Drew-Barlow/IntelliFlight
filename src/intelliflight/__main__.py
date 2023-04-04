a92483940f393e913e8daf3ed8eeff8add5211b2
Ethan Pelkie <epelk@umich.edu>
2023-04-04 13:21:00 -0400

    Add documentation to __main__.py

--- src/intelliflight/__main__.py
+++ src/intelliflight/__main__.py
@@ -8,9 +8,20 @@ from pydoc import pager
 from intelliflight.GUI import App
 
 
+## HELPER FUNCTIONS ##
+
+
 def cli_yes_no_prompt(msg: str) -> bool:
-    """Prompt the user for a y/n response with msg, returning True if 'y' is
-    answered and False if 'n' is answered."""
+    """Prompt the user with `msg` and await a y/n response.
+
+    `msg` will be printed in a loop until 'y' or 'n' is received as input.
+    Input is case-insensitive, but no extraneous characters (e.g., 'yes')
+    are acceptable.
+
+    Returns:
+
+    `True` if 'y'; `False` if 'n'
+    """
     response = ''
     while not (response == 'y' or response == 'n'):
         response = input(msg).lower()
@@ -19,7 +30,20 @@ def cli_yes_no_prompt(msg: str) -> bool:
 
 
 def ranged_float(min: float, max: float, inclusive_start: bool, inclusive_end: bool) -> callable:
-    """Argparse type representing a float between min and max."""
+    """Argparse type representing a float within a set range.
+
+    Positional Arguments:
+
+    - min -- lower bound for the acceptable range of values
+    - max -- upper bound for the acceptable range of values
+    - inclusive_start -- whether `min` is itself an acceptable value
+    - inclusive_end -- whether `max` is itself an acceptable value
+
+    Returns:
+
+    A function that can be passed to the `type` argument of
+    `argparse.add_argument()`
+    """
     def check_range(arg_val: float) -> float:
         """Function to check that the passed argument is in the range."""
         arg_f = None
@@ -47,7 +71,20 @@ def ranged_float(min: float, max: float, inclusive_start: bool, inclusive_end: b
 
 
 def time_str() -> callable:
-    """Argparse type representing a time string as hh:mm."""
+    """Argparse type representing a time string as hh:mm.
+
+    Hour and minute fields need not be padded to 2 characters each, but
+    each must be integral. Additionally, splitting the input string with ':'
+    must return exactly 2 tokens. The valid range for hours is [0,23]; the
+    valid range for minutes is [0,59].
+
+    Violation of the above conditions will result in an `ArgumentTypeError`.
+
+    Returns:
+
+    A function that can be passed to the `type` argument of
+    `argparse.add_argument()`
+    """
     def check_format(time: str) -> str:
         """Function to check that the passed argument is of correct format."""
         try:
@@ -75,14 +112,24 @@ def time_str() -> callable:
 
 
 class HelpfulParser(argparse.ArgumentParser):
-    """ArgumentParser class that prints the help"""
+    """ArgumentParser class designed to facilitate custom behavior when
+    invalid arguments are provided.
+
+    The `error()` method is overridden such that it will print an error
+    message before raising a generic `Exception()`. Calls to `parse_args()`
+    should be wrapped in a `try`-`except` block implementing custom behavior.
+    """
 
     def error(self, message):
         """Print error message and raise exception so main() can handle it."""
         print(f'Error: {message}\n')
         raise Exception()
 
-        # Setup argument parser
+
+## Argparse Setup ##
+
+
+# Setup argument parser
 parser = HelpfulParser(
     prog="IntelliFlight",
     description="An app to predict flight delays, diversions, and cancellations.",
@@ -175,7 +222,13 @@ list_subparser.add_argument(
     help='Input mapping to list.'
 )
 
+
+## App Logic ##
+
+
 if len(sys.argv) == 1:
+    # No command provided (agrv[0] is program name)
+    # Launch GUI
     App().mainloop()
 
 else:
@@ -204,6 +257,8 @@ else:
 
         exit()
 
+    # Args successfully parsed
+
     root_dir = Path(__file__).parent.parent.parent
 
     if sys.argv[1] == 'train':
@@ -217,12 +272,15 @@ else:
             print(f'Error: File {args.flight_path} does not exist.')
             exit()
 
+        # Train
         bayes = Bayes_Net()
         bayes.load_data(args.flight_path)
         k, accuracy = bayes.train_model(
             args.partition_count, args.k_step, args.max_k, args.rng_seed)
         print(
             f'Generated model parameters with k={k} and an estimated accuracy of {accuracy}%.')
+
+        # Prompt user to save model
         if cli_yes_no_prompt('Would you like to save this model? (y/n): '):
             bayes.export_parameters()
 
@@ -230,6 +288,7 @@ else:
         print('TODO: Implement CLI prediction')
 
     elif sys.argv[1] == 'list':
+        # Print input mappings
         if args.input == 'days':
             help_entries = [
                 'Listing days by ID...',
@@ -247,27 +306,37 @@ else:
         else:
             params_path = root_dir / 'data' / 'models' / 'bayes_net.model.json'
             if not params_path.exists():
+                # The model only supports predictions with airports/airlines
+                # seen in training data. Therefore, mappings are specific to
+                # trained model instances.
                 print('Error: The model must be trained to use this functionality.')
                 exit()
 
+            # Initialize model from which mappings will be extracted
             bayes = Bayes_Net(params_path)
 
             if args.input == 'airports':
+                # Get airport BTS IDs
                 airports = bayes.key_meta.get_seen_airports()
+                # Get airport mapping data
                 airport_map_path = root_dir / 'data' / 'maps' / 'airport_mappings.json'
                 mappings = json.load(airport_map_path.open())
+                # Generate 'description@id' strings for airports in model
                 mappings_pruned = [
                     f'{entry["desc"]}@{entry["bts_id"]}'
                     for entry in mappings
                     if entry['bts_id'] in airports
                 ]
+                # Get length of longest ID value (used for spacing later)
                 max_bts_len = 0
                 for entry in mappings_pruned:
                     current_bts = entry.split('@')[1]
                     if len(current_bts) > max_bts_len:
                         max_bts_len = len(current_bts)
 
+                # Sort airports alphabetically by name/description
                 mappings_pruned.sort()
+
                 help_entries = [
                     'Listing Airports by BTS ID...',
                     "Press [Enter] to proceed 1 line.",
@@ -275,31 +344,40 @@ else:
                     "Press [q] to exit.",
                     ''
                 ]
+                # Append 'ID: description' strings to help_entries
+                # Use max length found previously to pad 'ID:' portion and
+                # ensure consistent spacing between columns
                 for entry in mappings_pruned:
                     desc, bts = entry.split('@')
                     help_entries.append(
                         f'{(bts + ":").ljust(max_bts_len + 2, " ")}{desc}')
 
+                # Print paged output
                 pager('\n'.join(help_entries))
 
             else:
-                # Airlines
+                # Get airline codes
                 airlines = bayes.key_meta.get_seen_carriers()
+                # Get airline mapping data
                 airline_map_path = root_dir / 'data' / 'maps' / 'L_UNIQUE_CARRIERS.csv'
                 mappings = csv.DictReader(airline_map_path.open())
+                # Generate 'description@code' strings for airlines in model
                 mappings_pruned = [
                     f'{entry["Description"]}@{entry["Code"]}'
                     for entry in mappings
                     if entry['Code'] in airlines
                 ]
 
+                # Get length of longest code
                 max_code_len = 0
                 for entry in mappings_pruned:
                     current_code = entry.split('@')[1]
                     if len(current_code) > max_code_len:
                         max_code_len = len(current_code)
 
+                # Sort alphabetically by name/description
                 mappings_pruned.sort()
+
                 help_entries = [
                     'Listing Airlines by code...',
                     "Press [Enter] to proceed 1 line.",
@@ -307,14 +385,21 @@ else:
                     "Press [q] to exit.",
                     ''
                 ]
+                # Append 'Code: description' strings to help_entries
+                # Use max length found previously to pad 'Code:' portion and
+                # ensure consistent spacing between columns
                 for entry in mappings_pruned:
                     desc, code = entry.split('@')
                     help_entries.append(
                         f'{(code + ":").ljust(max_code_len + 2, " ")}{desc}')
 
+                # Print paged output
                 pager('\n'.join(help_entries))
 
     else:
+        # Command was not 'train', 'predict', or 'list'.
+        # The try-except block and len(argv) == 1 check above should make it
+        # impossible to enter this branch.
         raise Exception(
             'Something went wrong! This branch should never be triggered.')
 
